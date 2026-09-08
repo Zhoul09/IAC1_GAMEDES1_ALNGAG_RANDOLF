57262667dba0fc98a8ce7b760e13a1f1c30150fd
alngag_r <alngag_r@f111.ub.lan>
2026-09-08 13:20:54 +0800

    Initial commit

---
+++ player.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+var health = 100
+var player_name = "Reindeer"
+var defense = 1000
+var battle_cry = "KALBOOOOOOOOO!!!!!!!!"
+
+
+func _ready():
+	print(player_name, " has entered the scene with ", health, "HP. " , defense, " Defense.",)
+	print(battle_cry) 
+	
+	
+func take_damage(amount):
+	health -= amount
+	print("Health is now ", health)
+
+	
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
